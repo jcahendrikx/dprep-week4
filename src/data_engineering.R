558ff8b39ec8fe9fc335e4e908353aa64709ded2
Alex Hendrikx <j.c.a.hendrikx@tilburguniversity.edu>
2026-09-14 14:14:30 +0200

    Added Exercise 1

--- src/data_engineering.R
+++ src/data_engineering.R
@@ -1,9 +1,11 @@
 # Load the data
-
+video_view <- read_csv("data/video_view.csv")
+head(video_view)
 
 # Exercise 1
-
-
+video_view <- video_view %>% 
+  mutate(watch_rate_rank = rank(-watch_rate))
+head(video_view)
 
 # Exercise 2
 
