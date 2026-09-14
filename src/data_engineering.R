2cadfe6e98ad38c8074d38c43f8560addb789ae0
Alex Hendrikx <j.c.a.hendrikx@tilburguniversity.edu>
2026-09-14 14:21:25 +0200

    Finalized Exercise 1

--- src/data_engineering.R
+++ src/data_engineering.R
@@ -1,11 +1,23 @@
 # Load the data
+library(tidyverse)
 video_view <- read_csv("data/video_view.csv")
 head(video_view)
 
 # Exercise 1
-video_view <- video_view %>% 
-  mutate(watch_rate_rank = rank(-watch_rate))
-head(video_view)
+video_features <- video_view %>% 
+  mutate(watch_rate_rank = rank(-watch_rate)) %>% 
+  mutate(reach_band = case_when(
+    impressions_n < quantile(impressions_n, 1/3, na.rm = TRUE) ~ "Low",
+    impressions_n < quantile(impressions_n, 2/3, na.rm = TRUE) ~ "Medium",
+    TRUE                                                     ~ "High"
+  )) %>% 
+  mutate(high_quality = avg_watch_share >= 0.40) %>% 
+  distinct(video_id, .keep_all = TRUE) 
+
+write_csv(video_features, "temp/video_features.csv")
+head(video_features)
+video_features %>% select(impressions_n, reach_band)
+
 
 # Exercise 2
 
