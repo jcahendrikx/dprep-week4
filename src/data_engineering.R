b40d61866f7736fe0fdfaebfef851c2ad30b5e9e
Alex Hendrikx <j.c.a.hendrikx@tilburguniversity.edu>
2026-09-18 21:25:29 +0200

    Add timestamp conversion and lagged daily metrics

--- src/data_engineering.R
+++ src/data_engineering.R
@@ -109,4 +109,31 @@ creator_event_summary <- watch_log %>%
 write_csv(watch_log, "temp/watch_log.csv")
 write_csv(creator_event_summary, "temp/creator_event_summary.csv")
 
+# Example
+watch_time_preview <- watch_log %>%
+  mutate(
+    shown_ts = as.POSIXct(shown_at,
+                          format = "%Y-%m-%dT%H:%M:%SZ",
+                          tz = "UTC"),
+    shown_day = as.Date(shown_ts)
+  ) %>%
+  select(impression_id, creator_id, shown_at, shown_ts, shown_day) %>%
+  head(8)
+
+creator_daily <- watch_log %>%
+  mutate(shown_ts = as.POSIXct(shown_at, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")) %>% 
+  mutate(shown_day = as.Date(shown_ts)) %>%
+  count(creator_id, shown_day, name = "impressions_n") %>%
+  group_by(creator_id) %>%
+  arrange(shown_day) %>%
+  mutate(impressions_lag1 = lag(impressions_n))
+
 # Exercise 5
+creator_daily2 <- watch_log %>% 
+  mutate(shown_ts = as.POSIXct(shown_at, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")) %>% 
+  mutate(shown_day = as.Date(shown_ts)) %>%
+  count(creator_id, shown_day, name = "impressions_n") %>%
+  mutate(impressions_lag1 = lag(impressions_n)) %>% 
+  mutate(impressions_change = impressions_n - impressions_lag1)
+
+write_csv(creator_daily, "temp/creator_daily_week4.csv")
