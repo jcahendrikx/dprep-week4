b1055d015ecfce8b6faedd19782e9bb6aad067d6
Alex Hendrikx <j.c.a.hendrikx@tilburguniversity.edu>
2026-09-19 10:44:05 +0200

    Add  build scripts for temp and output tables

--- src/build_output.R
+++ src/build_output.R
@@ -3,3 +3,11 @@
 library(tidyverse)
 
 dir.create("output", showWarnings = FALSE)
+
+creator_week4 <- read_csv("temp/creator_week4.csv")
+
+creator_top10 <- creator_week4 %>% 
+  arrange(desc(impressions_total)) %>% 
+  head(10)
+
+write_csv(creator_top10, "output/creatpr_top10_week4.csv")
--- src/build_temp.R
+++ src/build_temp.R
@@ -3,3 +3,18 @@
 library(tidyverse)
 
 dir.create("temp", showWarnings = FALSE)
+
+video_view <- read_csv("data/video_view.csv")
+creators <- read_csv("data/creators.csv")
+
+creator_week4 <- video_view %>% 
+  left_join(creators, by = "creator_id") %>% 
+  group_by(creator_id) %>% 
+  summarise(
+    videos_n = n(),
+    impressions_total = sum(impressions_n, na.rm = TRUE),
+    watched_total = sum(watched_n, na.rm = TRUE),
+    avg_watch_rate = mean(watch_rate, na.rm = TRUE)
+  )
+write_csv(creator_week4, "temp/creator_week4.csv")
+
